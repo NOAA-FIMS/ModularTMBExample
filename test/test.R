249f1f0bbf88d5ead057d83cd9d4f503ea67a893
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2023-02-08 12:01:51 -0800

    Clean up test.R script and add some new options

--- test/test.R
+++ test/test.R
@@ -9,7 +9,7 @@ library(Rcpp)
 library(ModularTMBExample)
 library(dplyr)
 library(ggplot2)
-
+theme_set(theme_bw())
 
 ## simulate data, repeated measures for each fish so can use
 ## random effects
@@ -28,16 +28,19 @@ obs <- lapply(1:nfish, function(i)
         length=(exp(log_l_inf[i]))*(1.0-exp(-exp(log_k[i])*(ages-a_min))))) %>%
   bind_rows %>%
   mutate(obs=length*exp(rnorm(nfish*nreps,0-.05^2/2,.05)))
-
+g <- ggplot(obs, aes(age, length)) + geom_line() +
+  geom_point(mapping=aes(y=obs), col=2) +
+  facet_wrap('fish')
+g
 
 
 #get the Rcpp module
-gg <- Rcpp::Module(module = "growth",PACKAGE = "ModularTMBExample")
+model <- Rcpp::Module(module = "growth",PACKAGE = "ModularTMBExample")
 data <- list(obs=obs$obs, fish=obs$fish, age=obs$age)
 #clear the parameter list, if there already is one
-gg$clear();
+model$clear();
 #create a von Bertalanffy object
-vonB<-new(gg$vonBertalanffy)
+vonB<-new(model$vonBertalanffy)
 #initialize k
 vonB$log_k_mean$value<-log(.5)
 vonB$log_k_mean$estimable<-TRUE
@@ -62,24 +65,12 @@ vonB$predicted <- rep(0,len=nrow(obs))
 
 ### Have no random effects (turn off sigmas and RE vectors)
 vonB$prepare()
-parameters <- list(p = gg$get_parameter_vector())
+parameters <- list(p = model$get_parameter_vector())
 obj <- MakeADFun(data=list(), parameters, DLL="ModularTMBExample")
-obj$fn()
-str(obj$report(obj$par))
-obs$pred0 <- obj$report(obj$par)$pred
-vonB$finalize(obj$par)
-vonB$show()
-## optimize
 opt <- with(obj, nlminb(par, fn, gr))
-vonB$finalize(opt$par)
-vonB$show()
 obs$pred <- obj$report(opt$par)$pred
-g <- ggplot(obs, aes(age, length)) + geom_line() +
-  geom_point(mapping=aes(y=obs), col=2) +
-  geom_line(mapping=aes(y=pred0), col=3) +
-  geom_line(mapping=aes(y=pred), col=4) +
-  facet_wrap('fish')
-g
+sdreport(obj)
+g+ geom_line(data=obs, mapping=aes(y=pred), col=4)
 
 
 ## Penalized ML: turn on RE vectors but leave sigmas off at the
@@ -87,27 +78,35 @@ g
 vonB$log_l_inf$estimable <- rep(TRUE, nfish)
 vonB$log_k$estimable <- rep(TRUE, nfish)
 vonB$prepare()
-parameters <- list(p = gg$get_parameter_vector())
+parameters <- list(p = model$get_parameter_vector())
 obj <- MakeADFun(data=list(), parameters, DLL="ModularTMBExample")
-obj$fn()
 opt <- with(obj, nlminb(par, fn, gr))
-vonB$finalize(opt$par)
-vonB$show()
 obs$pred <- obj$report(opt$par)$pred
-g <- ggplot(obs, aes(age, length)) + geom_line() +
-  geom_point(mapping=aes(y=obs), col=2) +
-  geom_line(mapping=aes(y=pred0), col=3) +
-  geom_line(mapping=aes(y=pred), col=4) +
-  facet_wrap('fish')
-g
+sdreport(obj)
+g+ geom_line(data=obs, mapping=aes(y=pred), col=4)
 
 
 
 ## Turn on marginal ML estimation of Linf vector (but not k)
 vonB$log_l_inf_sigma$estimable <- TRUE
+parameters <- list(p = model$get_parameter_vector())
 
 ## Full RE estimation of both vectors
 vonB$log_k_sigma$estimable <- TRUE
+parameters <- list(p = model$get_parameter_vector())
+
+## Estimate blocks. Collapse vectors into sets of fixed effects
+vonB$log_k_sigma$estimable <- FALSE
+vonB$log_l_inf_sigma$estimable <- FALSE
+
+parameters <- list(p = model$get_parameter_vector())
+obj <- MakeADFun(data=list(), parameters, DLL="ModularTMBExample")
+opt <- with(obj, nlminb(par, fn, gr))
+obs$pred <- obj$report(opt$par)$pred
+sdreport(obj)
+g+ geom_line(data=obs, mapping=aes(y=pred), col=4)
+
+## Map off parts of a vector
 
 ## Likelihood profiling (via TMB?)
 
@@ -116,47 +115,7 @@ library(tmbstan)
 fit <- tmbstan(obj)
 print(fit)
 
-## Variational inference via tmbstan
-
-
-
-
-obj$gr()
-obj$fn()
-random <-  which(!1:length(obs$obs) %in% 1:4)
-obj$env$.random <- random
-obj$par <- obj$par[-random]
-obj$retape()
-## Check it worked
-obj$env$last.par.best
-obj$fn()
-obj$gr()
-
-## Fit model
-system.time(opt <- nlminb(obj$par, obj$fn, obj$gr))
-rep <- sdreport(obj)
-
-#update the von Bertalanffy object with updated parameters
-vonB$finalize(opt$par)
-
-#show results
-vonB$show()
-
-#show final gradient
-print("final gradient:")
-print(rep$gradient.fixed)
-
-obs$pred <- obj$report()$pred
-g <- ggplot(obs, aes(age, length)) + geom_line() +
-  geom_point(mapping=aes(y=obs), col=2) +
-  geom_line(mapping=aes(y=pred), col=3) +
-  facet_wrap('fish')
-g
-
-
-
-
-
+### Quick demo of the backdoor index approach to random effects
 runExample('simple')
 obj2 <- MakeADFun(data=list(x=x, B=B, A=A),
                  parameters=list(u=u*0, beta=beta*0, logsdu=1, logsd0=1),
@@ -167,3 +126,9 @@ obj2$par <- obj$par
 obj2$retape()
 opt2 <- nlminb(obj2$par, obj2$fn, obj2$gr)
 opt2$par-opt$par # yes it works
+obj2$report()
+obj$report()
+sdreport(obj)
+sdreport(obj2) # breaks!!
+
+fit <- tmbstan(obj2) ## works!
