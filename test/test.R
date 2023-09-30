5271056bad767347bdd6f2ae9b82510fff64dcf9
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2023-09-29 17:36:46 -0700

    feat: test gc() to clear pointers

--- test/test.R
+++ test/test.R
@@ -14,10 +14,10 @@ a_min<- 0.1
 k<- .5
 ages<-c(0.1, 1,2,3,4,5,6,7,8)
 #data<-c(replicate(length(ages), 0.0), 0.0)
-data<-replicate(length(ages), 0.0)
+sim.data<-replicate(length(ages), 0.0)
 
 for(i in 1:length(ages)){
-  data[i] = (l_inf * (1.0 - exp(-k * (ages[i] - a_min))))* runif(1,.90,1.5)
+  sim.data[i] = (l_inf * (1.0 - exp(-k * (ages[i] - a_min))))* runif(1,.90,1.5)
 }
 
 #clear the parameter list, if there already is one
@@ -40,7 +40,7 @@ vonB$l_inf$estimable<-TRUE
 
 #set data
 Dat <- new(g$ObsData) 
-Dat$Data <- data
+Dat$Data <- sim.data
 
 #set ages 
 Dat$ages<-ages
@@ -49,32 +49,72 @@ Dat$ages<-ages
 g$CreateModel()
 
 #create an empty data list (data set above)
-data <- list()
+DatList <- list()
 
 #create a parameter list
-parameters <- list(
+ParList <- list(
   p = g$get_parameter_vector()
 )
 
-obj <- MakeADFun(data, parameters, DLL="ModularTMBExample")
+obj <- MakeADFun(DatList, ParList, DLL="ModularTMBExample")
 newtonOption(obj, smartsearch=FALSE)
 
 print(obj$gr(obj$par))
 
 ## Fit model
 opt <- nlminb(obj$par, obj$fn, obj$gr)
+
 rep <- sdreport(obj)
 
 rep
 
-#update the von Bertalanffy object with updated parameters
-vonB$finalize(rep$par.fixed)
+#GC model and try again
+rm(g, vonB, Dat); invisible(gc())
+#get the Rcpp module
+g<-Rcpp::Module(module = "growth",PACKAGE = "ModularTMBExample")
 
-#show results
-vonB$show()
 
-obj$report()
+#create a von Bertalanffy object
+vonB<-new(g$vonBertalanffy)
 
-#show final gradient
-print("final gradient:")
-print(rep$gradient.fixed)
+#initialize k
+vonB$k$value<-.05
+vonB$k$estimable<-TRUE
+
+#initialize a_min
+vonB$a_min$value<-.01
+vonB$a_min$estimable<-FALSE
+
+#initialize l_inf
+vonB$l_inf$value<-7
+vonB$l_inf$estimable<-TRUE
+
+#set data
+Dat <- new(g$ObsData) 
+Dat$Data <- sim.data
+
+#set ages 
+Dat$ages<-ages
+
+#prepare for interfacing with TMB
+g$CreateModel()
+
+#create an empty data list (data set above)
+DatList <- list()
+
+#create a parameter list
+ParList <- list(
+  p = g$get_parameter_vector()
+)
+
+obj <- MakeADFun(DatList, ParList, DLL="ModularTMBExample")
+newtonOption(obj, smartsearch=FALSE)
+
+print(obj$gr(obj$par))
+
+## Fit model
+opt <- nlminb(obj$par, obj$fn, obj$gr)
+
+rep <- sdreport(obj)
+
+rep
