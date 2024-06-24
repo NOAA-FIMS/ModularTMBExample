d666c4e72a9ed7758b612aeb0daedd2aa763d91d
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2024-06-24 17:11:43 -0500

    move libraries to testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,5 +3,10 @@ library(testthat)
 library(TMB)
 library(Rcpp)
 library(ModularTMBExample)
+library(mvtnorm)
+library(tmbstan)
+library(ggplot2)
+library(rstantools)
+
 
 test_check("ModularTMBExample")
